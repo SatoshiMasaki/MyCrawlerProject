bd5f3dad5b51f1b1978aa1d63aeb4b2a307aa77a
SatoshiMasaki <42084481+SatoshiMasaki@users.noreply.github.com>
2021-06-13 23:33:36 +0900

    Delete myCrawlerProject directory

--- myCrawlerProject/alkana/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-#
-# Licence GPLv2
-# (c) 2019 cod
-
-
-from .main import get_kana
-from .external_data import add_external_data
--- myCrawlerProject/memo.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    test_tuple = str((4, 5, 2, 5))
-    print(test_tuple)
-    print(type(test_tuple))
